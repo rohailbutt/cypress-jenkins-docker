47f4e8efd82947b6fe68aa0a3ec9ee6edc6b3b4b
Rohail Butt <rohailkinectro@gmail.com>
2022-02-06 23:23:21 +0500

    jenkins

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -2,17 +2,4 @@ describe("example to-do app", () => {
   it("it should get the logged in userq", () => {});
   it("it should get the logged in userq]d", () => {});
   it("it should get the logged in userf", () => {});
-  it("it should get the logged in user", () => {
-    cy.request({
-      method: "POST",
-      url: "auth/login",
-      body: {
-        email: `admin@amotius.com`,
-        password: `kin@123456`,
-      },
-    }).then((res) => {
-      expect(res.status).to.eq(200);
-      cy.log(res.body.user.token);
-    });
-  });
 });
